ad4ecbaf35be1b3ee26ede77090354d3b80ea971
Giovani Frota <135627043+giovanifrota@users.noreply.github.com>
2025-04-09 22:46:38 -0300

    aprendendo a criar filhos e heranças no JS

--- 4299-javascript-projeto-base/4299-javascript-projeto-base/index.js
+++ 4299-javascript-projeto-base/4299-javascript-projeto-base/index.js
@@ -1,6 +1,10 @@
 const inputItem = document.getElementById("input-item")
 
+const listaDeCompras = document.getElementById("lista-de-compras")
+
 const botaoAdicionar = document.getElementById("adicionar-item")
+
+let contador = 0;
 
 botaoAdicionar.addEventListener("click", (evento)=> {
     evento.preventDefault();
@@ -9,8 +13,19 @@ botaoAdicionar.addEventListener("click", (evento)=> {
         return
     }
 
-
     const itemDaLista = document.createElement("li");
     const containerItemDaLista = document.createElement("div");
+    const inputCheckbox = document.createElement("input");
+    const nomeItem = document.createElement("p");
     containerItemDaLista.classList.add("lista-item-container");
+    inputCheckbox.type = "checkbox";
+    inputCheckbox.id = "checkbox-" + contador++;
+    nomeItem.innerText = inputItem.value;
+
+    containerItemDaLista.appendChild(inputCheckbox);
+    containerItemDaLista.appendChild(nomeItem);
+
+    itemDaLista.appendChild(containerItemDaLista);
+
+    listaDeCompras.appendChild(itemDaLista);
 })
